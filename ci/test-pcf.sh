aec245cbf620aa5861ab49d52a9fdd1176d96998
Winston Rast <winston@rast.org>
2017-08-29 15:26:02 -0600

    switch to govpaas docker image

--- ci/test-pcf.sh
+++ ci/test-pcf.sh
@@ -5,27 +5,30 @@
 # echo "installing sudo"
 # apt-get install sudo
 
-echo "install ruby"
-apt-get -y install ruby-full
+# echo "install ruby"
+# apt-get -y install ruby-full
 
 # BOSH
 # echo "installing bosh"
 # gem install bosh_cli --no-ri --no-rdoc
-#
-# # CF
+
+curl -L "https://cli.run.pivotal.io/stable?release=linux64-binary" | tar -zx -C /usr/local/bin
+# curl -L 'https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64' -o /usr/local/bin/jq && chmod +x /usr/local/bin/jq
+
+# CF
 # echo "installing cf"
 # wget -q -O - https://packages.cloudfoundry.org/debian/cli.cloudfoundry.org.key | apt-key add -
 # echo "deb http://packages.cloudfoundry.org/debian stable main" | tee /etc/apt/sources.list.d/cloudfoundry-cli.list
 # apt-get -y update
 # apt-get -y install cf-cli
-#
-# # UAAC
-# echo "installing uaac"
-# gem install cf-uaac
 
-# jq
-# echo "installing jq"
-# sudo apt-get install jq
+# UAAC
+echo "installing uaac"
+gem install cf-uaac
+
+jq
+echo "installing jq"
+apt-get install jq
 
 # Do something useful
 
