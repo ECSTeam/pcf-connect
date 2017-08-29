47b2a104542fdb20f1b781fa019836e16da67aa9
Winston Rast <winston@rast.org>
2017-08-29 12:20:44 -0600

    use ubuntu docker image

--- ci/test-pcf.sh
+++ ci/test-pcf.sh
@@ -2,34 +2,32 @@
 
 # get "pcf" script required command line utilities
 
-echo "looking good!"
-
-which gem
+echo "installing bosh"
 
 # BOSH
-# gem install bosh_cli --no-ri --no-rdoc
-#
-# # CF
-# wget -q -O - https://packages.cloudfoundry.org/debian/cli.cloudfoundry.org.key | sudo apt-key add -
-# echo "deb http://packages.cloudfoundry.org/debian stable main" | sudo tee /etc/apt/sources.list.d/cloudfoundry-cli.list
-# sudo apt-get update
-# sudo apt-get install cf-cli
-#
-# # ...first configure the Cloud Foundry Foundation package repository
-# # sudo wget -O /etc/yum.repos.d/cloudfoundry-cli.repo https://packages.cloudfoundry.org/fedora/cloudfoundry-cli.repo
-# # sudo yum install cf-cli
-#
-# # UAAC
-# gem install cf-uaac
-#
-# # jq
-# sudo apt-get install jq
-#
-# # Do something useful
-#
-# cmd=./pcf-repo/pcf
-#
-# $cmd alias test-name -n $OPSMAN_HOST -u $OPSMAN_USERNAME -p $OPSMAN_PASSWORD -o $CF_ORG -s $CF_SPACE
-#
-# $cmd target test-name
-# $cmd targets
+gem install bosh_cli --no-ri --no-rdoc
+
+# CF
+echo "installing cf"
+wget -q -O - https://packages.cloudfoundry.org/debian/cli.cloudfoundry.org.key | sudo apt-key add -
+echo "deb http://packages.cloudfoundry.org/debian stable main" | sudo tee /etc/apt/sources.list.d/cloudfoundry-cli.list
+sudo apt-get update
+sudo apt-get install cf-cli
+
+# UAAC
+echo "installing uaac"
+gem install cf-uaac
+
+# jq
+echo "installing jq"
+sudo apt-get install jq
+
+# Do something useful
+
+echo "running tests"
+cmd=./pcf-repo/pcf
+
+$cmd alias test-name -n $OPSMAN_HOST -u $OPSMAN_USERNAME -p $OPSMAN_PASSWORD -o $CF_ORG -s $CF_SPACE
+
+$cmd target test-name
+$cmd targets
