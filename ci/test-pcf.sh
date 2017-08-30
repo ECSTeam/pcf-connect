be6c56233970d66eb948c53b7bf9f99ee41bef48
Winston Rast <winston@rast.org>
2017-08-30 11:57:38 -0600

    silent output for prereqs

--- ci/test-pcf.sh
+++ ci/test-pcf.sh
@@ -4,14 +4,14 @@
 
 # BOSH
 echo "installing bosh"
-gem install bosh_cli --no-ri --no-rdoc
+gem install bosh_cli --silent --no-ri --no-rdoc
 
 # gem install apk
 
 # CF
 echo "installing cf"
-gem install curl
-curl -L "https://cli.run.pivotal.io/stable?release=linux64-binary" | tar -zx -C /usr/local/bin
+gem install curl --silent
+curl -s -L "https://cli.run.pivotal.io/stable?release=linux64-binary" | tar -zx -C /usr/local/bin
 
 # wget -q -O - https://packages.cloudfoundry.org/debian/cli.cloudfoundry.org.key | apt-key add -
 # echo "deb http://packages.cloudfoundry.org/debian stable main" | tee /etc/apt/sources.list.d/cloudfoundry-cli.list
@@ -20,11 +20,11 @@ curl -L "https://cli.run.pivotal.io/stable?release=linux64-binary" | tar -zx -C 
 
 # UAAC
 echo "installing uaac"
-gem install cf-uaac
+gem install cf-uaac --silent
 
 # jq
 echo "installing jq"
-curl -L 'https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64' -o /usr/local/bin/jq && chmod +x /usr/local/bin/jq
+curl -s -L 'https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64' -o /usr/local/bin/jq && chmod +x /usr/local/bin/jq
 # apt-get install jq
 
 # Do something useful
