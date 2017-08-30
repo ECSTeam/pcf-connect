9805555120092395ba69f8c5ada25965eed6affb
Winston Rast <winston@rast.org>
2017-08-30 09:51:07 -0600

    cleanup

--- ci/test-pcf.sh
+++ ci/test-pcf.sh
@@ -1,16 +1,6 @@
-#!/bin/bash -ex
+#!/bin/bash -e
 
-# get "pcf" script required command line utilities
-
-# echo "installing sudo"
-# apt-get install sudo
-
-# echo "install ruby"
-# apt-get -y install ruby-full
-
-# for debugging
-apt-get update
-apt-get -y install vim
+# get "pcf" script required command line utilities - at some point just make a new docker image
 
 # BOSH
 echo "installing bosh"
@@ -19,10 +9,10 @@ gem install bosh_cli --no-ri --no-rdoc
 # gem install apk
 
 # CF
+echo "installing cf"
 gem install curl
 curl -L "https://cli.run.pivotal.io/stable?release=linux64-binary" | tar -zx -C /usr/local/bin
 
-# echo "installing cf"
 # wget -q -O - https://packages.cloudfoundry.org/debian/cli.cloudfoundry.org.key | apt-key add -
 # echo "deb http://packages.cloudfoundry.org/debian stable main" | tee /etc/apt/sources.list.d/cloudfoundry-cli.list
 # apt-get -y update
@@ -43,6 +33,5 @@ echo "running tests"
 cmd=./pcf-repo/pcf
 
 $cmd alias test-name -n $OPSMAN_HOST -u $OPSMAN_USERNAME -p $OPSMAN_PASSWORD -o $CF_ORG -s $CF_SPACE
-
 $cmd target test-name
 $cmd targets
