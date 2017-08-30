d84e22b34dbfa879087f164110428635832dbd32
Steve Wall <swall@ecsteam.com>
2017-08-30 13:34:20 -0600

    Update test-pcf.sh
    Commented out code removed.

--- ci/test-pcf.sh
+++ ci/test-pcf.sh
@@ -6,17 +6,10 @@
 echo "installing bosh"
 gem install bosh_cli --silent --no-ri --no-rdoc
 
-# gem install apk
-
 # CF
 echo "installing cf"
 gem install curl --silent
 curl -s -L "https://cli.run.pivotal.io/stable?release=linux64-binary" | tar -zx -C /usr/local/bin
-
-# wget -q -O - https://packages.cloudfoundry.org/debian/cli.cloudfoundry.org.key | apt-key add -
-# echo "deb http://packages.cloudfoundry.org/debian stable main" | tee /etc/apt/sources.list.d/cloudfoundry-cli.list
-# apt-get -y update
-# apt-get -y install cf-cli
 
 # UAAC
 echo "installing uaac"
@@ -25,7 +18,6 @@ gem install cf-uaac --silent
 # jq
 echo "installing jq"
 curl -s -L 'https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64' -o /usr/local/bin/jq && chmod +x /usr/local/bin/jq
-# apt-get install jq
 
 # Do something useful
 
