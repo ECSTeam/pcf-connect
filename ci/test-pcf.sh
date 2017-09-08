5a2a4ec9d8a7450f11c5c08723ad5ae50d2c8f12
Winston Rast <winston@rast.org>
2017-09-08 11:50:40 -0600

    add test for bad creds

--- ci/test-pcf.sh
+++ ci/test-pcf.sh
@@ -20,10 +20,16 @@ echo "installing jq"
 curl -s -L 'https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64' -o /usr/local/bin/jq && chmod +x /usr/local/bin/jq
 
 # Do something useful
-
-echo "running tests using bosh v1"
 cmd=./pcf-repo/pcf
 
+echo "test with bad credentials"
+result=`$cmd alias bad-creds -n $OPSMAN_HOST -u wronguser -p wrongpassword`
+if [[ ! $result == Error* ]]; then
+  echo "Bad credentials did not fail properly"
+  exit 1
+fi
+
+echo "running tests using bosh v1"
 $cmd alias test-name -n $OPSMAN_HOST -u $OPSMAN_USERNAME -p $OPSMAN_PASSWORD -o $CF_ORG -s $CF_SPACE
 $cmd target test-name
 $cmd targets
@@ -36,8 +42,6 @@ yes | gem uninstall bosh_cli --silent
 curl -s -L 'https://s3.amazonaws.com/bosh-cli-artifacts/bosh-cli-2.0.28-linux-amd64' -o /usr/local/bin/bosh && chmod +x /usr/local/bin/bosh
 
 echo "running tests using bosh v2"
-cmd=./pcf-repo/pcf
-
 $cmd alias test-name -n $OPSMAN_HOST -u $OPSMAN_USERNAME -p $OPSMAN_PASSWORD -o $CF_ORG -s $CF_SPACE
 $cmd target test-name
 $cmd targets
